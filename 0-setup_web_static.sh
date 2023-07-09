d74cfef1fe7557fcfc1d1795fec254d093184a6a
Emeka Emodi <emodiemeka@gmail.com>
2023-07-08 22:10:24 -0700

    Configure web server for web_static deployment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,27 +1,37 @@
 #!/usr/bin/env bash
-##############################################################
-# Script name: 0-setup_web_static.sh
+################################################################################
+# Script Name: 0-setup_web_static.sh
 # Description: Sets up a web server for web_static deployment.
-##############################################################
+################################################################################
 
 # Install Nginx if it is not already installed
 if ! command -v nginx &> /dev/null; then
-   sudo apt-get update
-   sudo apt-get -y install nginx
+    sudo apt-get update
+    sudo apt-get -y install nginx
 fi
 
 # Create necessary directories if they don't already exist
 sudo mkdir -p /data/web_static/{releases/test,shared}
 
 # Create a fake HTML file for testing
-sudo echo "Test Page" | sudo tee /data/web_static/releases/test/index.html
+html_content=$(cat <<EOF
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+EOF
+)
+echo "$html_content" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
-# Create or recreate the symbolink link
+# Create or recreate the symbolic link
 sudo rm -rf /data/web_static/current
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Grant ownership of the /data/ directory to ubuntu user and group
-sudo chown -R ubuntu /data/
+# Give ownership of the /data/ folder to ubuntu user and group recursively
+sudo chown -R ubuntu:ubuntu /data/
 
 # Update Nginx configuration
 config_file="/etc/nginx/sites-available/default"
