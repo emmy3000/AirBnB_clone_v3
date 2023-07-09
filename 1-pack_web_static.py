8d867fa2200e5a6f5414f793d98644e3a5478d4d
Emeka Emodi <emodiemeka@gmail.com>
2023-07-08 23:59:01 -0700

    Updated web_static dir

---
+++ 1-pack_web_static.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This script creates a compressed archive of the web_static folder.
+"""
+
+import os
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """
+    Creates a compressed archive of the web_static folder.
+
+    Returns:
+        str: Path of the generated archive if successful, None otherwise.
+    """
+    current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_name = "web_static_" + current_time + ".tgz"
+    archive_path = "versions/" + archive_name
+
+    try:
+        if not os.path.exists("versions"):
+            os.makedirs("versions")
+
+        local("tar -czvf {} web_static".format(archive_path))
+        return archive_path
+    except Exception:
+        return None
---
+++ web_static/README.md
@@ -0,0 +1,3 @@
+# AirBnB clone - Web static
+
+Airbnb objects brought to life by building a static frontend application.
