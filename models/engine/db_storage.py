8829515e4aed19847304c759559575d6ca6cfa66
Emeka Emodi <emodiemeka@gmail.com>
2023-07-23 07:51:22 -0700

    Database Storage Module Updated

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -17,7 +17,7 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from sqlalchemy import create_engine
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship, scoped_session, sessionmaker
 
 
