1ecf99a14f57ad3e7c57c20c271a326f672f9cc2
Emeka Emodi <emodiemeka@gmail.com>
2023-07-23 12:29:48 -0700

    Database Storage Module Updated

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,31 +1,44 @@
 #!/usr/bin/python3
 """DBStorage Engine Module
 
-    This module defines the DBStorage engine used for database storage.
-    It provides methods for querying, adding, and deleting objects
-    from the database.
+This module defines the DBStorage engine used for database storage.
+It provides methods for querying, adding, and deleting objects
+from the database.
 
-    Classes:
+Classes:
     DBStorage: Represents the database storage engine.
 """
 
 from os import getenv
-from models.base_model import Base, BaseModel
+from models.base_model import Base
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from sqlalchemy import Column, String
+from sqlalchemy import create_engine
 from sqlalchemy.orm import relationship, scoped_session, sessionmaker
 
 
 class DBStorage:
+    """
+    Represents the database storage engine.
+
+    Attributes:
+        __engine (sqlalchemy.Engine): SQLAlchemy engine for database connection.
+        __session (sqlalchemy.Session): SQLAlchemy session to interact with the database.
+    """
+
     __engine = None
     __session = None
 
     def __init__(self):
+        """Initialize a new DBStorage instance.
+
+        The constructor creates a new database engine and sets up the session.
+        If the environment is set to 'test', it drops all tables and recreates them.
+        """
         self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
                                       format(getenv("HBNB_MYSQL_USER"),
                                              getenv("HBNB_MYSQL_PWD"),
@@ -36,6 +49,15 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
+        """Query objects from the database.
+
+        Args:
+            cls (str or SQLAlchemy class, optional): The class name or class to query.
+                Defaults to None, which retrieves all objects from all classes.
+
+        Returns:
+            dict: A dictionary of objects with format '{ClassName}.{object_id}'.
+        """
         if cls is None:
             objs = self.__session.query(State).all()
             objs.extend(self.__session.query(City).all())
@@ -44,41 +66,39 @@ class DBStorage:
             objs.extend(self.__session.query(Review).all())
             objs.extend(self.__session.query(Amenity).all())
         else:
-            if type(cls) == str:
+            if isinstance(cls, str):
                 cls = eval(cls)
             objs = self.__session.query(cls)
         return {"{}.{}".format(type(o).__name__, o.id): o for o in objs}
 
     def new(self, obj):
+        """Add a new object to the current database session.
+
+        Args:
+            obj (BaseModel): The object to add to the session.
+        """
         self.__session.add(obj)
 
     def save(self):
+        """Commit changes to the current database session."""
         self.__session.commit()
 
     def delete(self, obj=None):
+        """Delete an object from the database session.
+
+        Args:
+            obj (BaseModel, optional): The object to delete from the session.
+        """
         if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
+        """Create all tables in the database and create a new session."""
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine,
-                                       expire_on_commit=False)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
 
     def close(self):
+        """Close the current session."""
         self.__session.close()
-
-
-class State(BaseModel, Base):
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-
-    @property
-    def cities(self):
-        cities_list = []
-        all_cities = models.storage.all(City)
-        for city in all_cities.values():
-            if city.state_id == self.id:
-                cities_list.append(city)
-        return cities_list
