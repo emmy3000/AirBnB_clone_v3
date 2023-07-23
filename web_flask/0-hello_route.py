63dee24f745ce0ae51ac0a4ac9e575da725f62c6
Emeka Emodi <emodiemeka@gmail.com>
2023-07-23 01:56:17 -0700

    feat: Add route for "Hello HBNB!"

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Simple Flask webapplication that displayes "Hello HBNB!".
+The application listens on 0.0.0.0, port 5000.
+
+Routes:
+    /: display "Hello HBNB!"
+    Mandatory use of `strict_slashes=False` in route definition.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Displays "Hello HBNB!" when accessing the root URL.
+    """
+    return 'Hello HBNB!'
+
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port=5000)
