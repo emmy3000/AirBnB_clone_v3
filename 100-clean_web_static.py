77405e107c1b9b91bec8faa984bb239bef33fad4
Emeka Emodi <emodiemeka@gmail.com>
2023-07-26 08:50:07 -0700

    Fix do_clean() function in Fabric script for deleting out-of-date archives

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -5,92 +5,35 @@ Script name: 100-clean_web_static.py
 This script deletes out-dated archives by
 utilizing the do_clean() function.
 """
-
 import os
-from fabric.api import env, run, local, put
-from datetime import datetime
-from fabric.context_managers import lcd
+from fabric.api import env, run, local
 
 # IP addresses for the web servers assigned to an env variable
 env.hosts = ['54.209.192.188', '54.89.30.164']
 
 
-def do_pack():
-    """Create a compressed archive of the web_static folder."""
-    try:
-        timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-        archive_name = "versions/web_static_{}.tgz".format(timestamp)
-        local("mkdir -p versions")
-        local("tar -czvf {} web_static".format(archive_name))
-        return archive_name
-    except Exception:
-        return None
-
-
-def do_deploy(archive_path):
-    """Distribute an archive to your web servers."""
-    if not os.path.exists(archive_path):
-        return False
-
-    try:
-        file = os.path.basename(archive_path)
-        name = os.path.splitext(file)[0]
-
-        with settings(warn_only=True):
-            if put(archive_path, "/tmp/{}".format(file)).failed:
-                return False
-
-            if run("mkdir -p /data/web_static/releases/{}/"
-                   .format(name)).failed:
-                return False
-
-            if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-                   .format(file, name)).failed:
-                return False
-
-            if run("mv /data/web_static/releases/{}/web_static/* "
-                   "/data/web_static/releases/{}/"
-                   .format(name, name)).failed:
-                return False
-
-            if run("rm /tmp/{}".format(file)).failed:
-                return False
-
-            if run("rm -rf /data/web_static/releases/{}/web_static"
-                   .format(name)).failed:
-                return False
-
-            if run("rm -rf /data/web_static/current").failed:
-                return False
-
-            if run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-                   .format(name)).failed:
-                return False
-
-        return True
-    except Exception:
-        return False
-
-
-def deploy():
-    """Create and distribute the archive to the web servers."""
-    archive_path = do_pack()
-    if archive_path is None:
-        return False
-
-    return do_deploy(archive_path)
-
-
 def do_clean(number=0):
-    """Delete out-of-date archives."""
+    """
+    Delete out-dated archives.
+
+    Args:
+        number (int): The number of archives retained.
+
+    If number == 0 || 1, retains only the most recent archive.
+    If number == 2, retains either most recent || 2nd most recent archives
+    """
     number = int(number)
-    if number < 0:
-        return
-    elif number == 0:
-        number = 1
+    number = 1 if number < 1 else number
 
-    with lcd("versions"):
-        local("ls -t | tail -n +{} | xargs -d '\n' rm -f".format(number + 1))
+    # Local clean up
+    os.chdir("./versions")
+    archives = sorted(os.listdir("."))
+    archives = archives[:-number]
+    [local("rm {}".format(a)) for a in archives]
 
+    # Remote clean up
     with cd("/data/web_static/releases"):
-        run("ls -t | tail -n +{} | xargs -d '\n' rm -rf".format(number + 1))
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        archives = archives[:-number]
+        [run("rm -rf {}".format(a)) for a in archives]
