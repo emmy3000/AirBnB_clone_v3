ff456036cd9ba475361e29c14ef23b833ebc5cea
Emeka Emodi <emodiemeka@gmail.com>
2023-07-26 11:29:33 -0700

    User Module

--- models/user.py
+++ models/user.py
@@ -2,19 +2,18 @@
 """The User class module"""
 
 from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import String
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class User(BaseModel, Base):
+class User(Base):
     """Represents MySQL database for class User.
 
     Inherits from SQLAlchemy Base & links to MySQL table users.
 
     Attributes:
         __tablename__ (str): name of the MySQL table for users.
+        id: (sqlalchemy String): user's id (primary key). Change to String(60).
         email: (sqlalchemy String): user's email address.
         password (sqlalchemy String): user's password.
         first_name (sqlalchemy String): user's first name.
@@ -23,9 +22,12 @@ class User(BaseModel, Base):
         reviews (sqlalchemy-relationship): User-Review relationship.
     """
     __tablename__ = "users"
+    id = Column(String(60), primary_key=True, nullable=False)
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
     places = relationship("Place", backref="user", cascade="delete")
     reviews = relationship("Review", backref="user", cascade="delete")
+
+    __table_args__ = {'mysql_charset': 'latin1'}
