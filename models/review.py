a36dd8fd43fe717b3797115b00bfad44df180006
Emeka Emodi <emodiemeka@gmail.com>
2023-07-26 11:29:06 -0700

    Review Module

--- models/review.py
+++ models/review.py
@@ -2,25 +2,26 @@
 """The Review class module"""
 
 from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import ForeignKey
-from sqlalchemy import String
+from sqlalchemy import Column, ForeignKey, String, Integer
 from sqlalchemy.orm import relationship
 
 
-class Review(BaseModel, Base):
-    """Represents a review objects for MySQL database.
+class Review(Base):
+    """Represents a review object for MySQL database.
 
-    Inherits from SQLAlchemy Base & links the MySQL table review objects..
+    Inherits from SQLAlchemy Base & links the MySQL table review objects.
 
     Attributes:
         __tablename__ (str): name of the MySQL table to store Reviews.
+        id (sqlalchemy String): primary key column. Change to String(60).
         text (sqlalchemy String): review class' description.
         place_id (sqlalchemy String): review class' place id.
         user_id (sqlalchemy String): review class' user id.
     """
     __tablename__ = "reviews"
+    id = Column(Integer, primary_key=True, nullable=False)
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+
+    __table_args__ = {'mysql_charset': 'latin1'}
