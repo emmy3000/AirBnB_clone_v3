7617745d8316a2dddb3e8461bc67df8cbc7bdb60
Emeka Emodi <emodiemeka@gmail.com>
2023-07-29 05:18:20 -0700

    Create hbnb_test_db and grant privileges to hbnb_test user

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,18 @@
 -- Prepares a MySQL Test server for the project.
+
+-- Revoke existing privileges if any
+REVOKE ALL PRIVILEGES ON hbnb_test_db.* FROM 'hbnb_test'@'localhost';
+REVOKE SELECT ON performance_schema.* FROM 'hbnb_test'@'localhost';
+
+-- Create the database if not exists
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create the user if not exists
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
+
+-- Grant privileges to the user
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Flush privileges to apply the changes
 FLUSH PRIVILEGES;
