ed9ba438a05517ed71dc82099204f038b499715a
Emeka Emodi <emodiemeka@gmail.com>
2023-07-29 05:21:44 -0700

    Amenity Module

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """The Amenity class module"""
 
-from models.base_model import Base
+from datetime import datetime
+from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String, Integer
 
 
-class Amenity(Base):
+class Amenity(BaseModel, Base):
     """Represents Amenity class for MySQL database.
 
     Inherits from SQLAlchemy Base & links to MySQL amenities table.
@@ -20,3 +21,27 @@ class Amenity(Base):
     id = Column(String(60), primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
     __table_args__ = {'mysql_default_charset': 'latin1'}
+
+    def __init__(self, *args, **kwargs):
+        """Initialize Amenity object.
+
+        Call parent's (BaseModel's) __init__ to apply BaseModel behavior.
+        """
+        super().__init__(*args, **kwargs)
+
+    def save(self):
+        """Save the current instance to the database."""
+        from models import storage
+        self.updated_at = datetime.now()
+        storage.new(self)
+        storage.save()
+
+    def to_dict(self):
+        """Return a dictionary representation of the Amenity instance."""
+        return {
+            "id": self.id,
+            "name": self.name,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat(),
+            "__class__": self.__class__.__name__,
+        }
