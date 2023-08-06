affd01d36c7d44513a1e4b4eb6aaabc600403d2e
Emeka Emodi <emodiemeka@gmail.com>
2023-08-06 01:47:07 -0700

    Integrator: Initiate models package with storage engine selection.

--- models/__init__.py
+++ models/__init__.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""Object storage instantiation
-
-    * If environment variable `HBNB_TYPE_STORAGE` is set to `db`,
-    instantiates a database storage engine (DBStorage).
-    * Otherwise, instantiates a file storage engine (FileStorage).
+"""
+Initialize the models package based on the storage type.
 """
 
 from os import getenv
 from models.engine.db_storage import DBStorage
 from models.engine.file_storage import FileStorage
 
-if getenv("HBNB_TYPE_STORAGE") == "db":
+# Get the storage type from the environment variable
+storage_type = getenv("HBNB_TYPE_STORAGE")
+
+# Initialize the storage based on the storage type
+if storage_type == "db":
     storage = DBStorage()
 else:
     storage = FileStorage()
 
+# Load the data from the storage
 storage.reload()
