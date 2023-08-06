8e077dcb90257326fb91eb57ff60d973d8827c44
Emeka Emodi <emodiemeka@gmail.com>
2023-08-06 03:02:40 -0700

    feat: Implement User class for database storage

--- models/user.py
+++ models/user.py
@@ -1,58 +1,71 @@
 #!/usr/bin/python3
-"""The User class module"""
+"""
+This module defines the User class, representing a user entity in the database.
+"""
 
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.orm import relationship
 from datetime import datetime
+from hashlib import md5
 
 
 class User(BaseModel, Base):
-    """Represents MySQL database for class User.
+    """
+    Represents User class for the MySQL database.
 
-    Inherits from SQLAlchemy Base & links to MySQL table users.
+    Inherits from BaseModel and links to the MySQL table 'users'.
 
     Attributes:
-        __tablename__ (str): name of the MySQL table for users.
-        id: (sqlalchemy String): user's id (primary key). Change to String(60).
-        email: (sqlalchemy String): user's email address.
-        password (sqlalchemy String): user's password.
-        first_name (sqlalchemy String): user's first name.
-        last_name (sqlalchemy String): user's last name.
-        places (sqlalchemy-relationship): User-Place relationship.
-        reviews (sqlalchemy-relationship): User-Review relationship.
+        __tablename__ (str): The name of the MySQL table for users.
+        id (str): The user's id (primary key).
+        email (str): The user's email address.
+        password (str): The user's password.
+        first_name (str): The user's first name.
+        last_name (str): The user's last name.
+        created_at (DateTime): Datetime when the user is created.
+        updated_at (DateTime): Datetime when the user is updated.
+        places (relationship): User-Place relationship.
+        reviews (relationship): User-Review relationship.
     """
+
     __tablename__ = "users"
     id = Column(String(60), primary_key=True, nullable=False)
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-
-    # Add new columns
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
     updated_at = Column(DateTime, nullable=False,
                         default=datetime.utcnow, onupdate=datetime.utcnow)
 
-    # Existing relationships
     places = relationship("Place", backref="user", cascade="delete")
     reviews = relationship("Review", backref="user", cascade="delete")
 
     __table_args__ = {'mysql_charset': 'latin1'}
 
     def save(self):
-        """Updates updated_at attribute and saves the instance"""
+        """
+        Updates updated_at attribute and saves the instance.
+
+        If the password is not hashed (less than 32 characters), it will be hashed.
+        """
+        if self.password and len(str(self.password)) < 32:
+            self.password = md5(self.password.encode()).hexdigest()
         super().save()
 
     def to_dict(self):
-        """Return a dictionary representation of the User object."""
-        user_dict = {
-            'id': self.id,
-            'email': self.email,
-            'password': self.password,
-            'first_name': self.first_name,
-            'last_name': self.last_name,
-            'created_at': self.created_at.isoformat(),
-            'updated_at': self.updated_at.isoformat(),
-        }
+        """
+        Return a dictionary representation of the User object.
+
+        Returns:
+            dict: Dictionary representation of the User object.
+        """
+        user_dict = super().to_dict()
+        user_dict["email"] = self.email
+        user_dict["password"] = self.password
+        user_dict["first_name"] = self.first_name
+        user_dict["last_name"] = self.last_name
+        user_dict["created_at"] = self.created_at.isoformat()
+        user_dict["updated_at"] = self.updated_at.isoformat()
         return user_dict
