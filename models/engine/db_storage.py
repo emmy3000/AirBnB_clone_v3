30110608fd4ec7a0c377fbeda736e47c0bf6ff07
Emeka Emodi <emodiemeka@gmail.com>
2023-08-06 01:34:02 -0700

    feat: Database Storage Module [Updated]

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""DBStorage Engine Module
+"""DBStorage Engine Module.
 
 This module defines the DBStorage engine used for database storage.
 It provides methods for querying, adding, and deleting objects
@@ -19,6 +19,7 @@ from models.state import State
 from models.user import User
 from sqlalchemy import create_engine
 from sqlalchemy.orm import relationship, scoped_session, sessionmaker
+import pymysql
 
 
 class DBStorage:
@@ -27,9 +28,9 @@ class DBStorage:
 
     Attributes:
         __engine (sqlalchemy.Engine): SQLAlchemy engine
-        for database connection.
+            for database connection.
         __session (sqlalchemy.Session): SQLAlchemy session
-        to interact with the database.
+            to interact with the database.
     """
 
     __engine = None
@@ -43,7 +44,7 @@ class DBStorage:
         If the environment is set to 'test', it drops all tables
         and recreates them.
         """
-        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
+        self.__engine = create_engine("mysql+pymysql://{}:{}@{}/{}".
                                       format(getenv("HBNB_MYSQL_USER"),
                                              getenv("HBNB_MYSQL_PWD"),
                                              getenv("HBNB_MYSQL_HOST"),
@@ -51,29 +52,28 @@ class DBStorage:
                                       pool_pre_ping=True)
         if getenv("HBNB_ENV") == "test":
             Base.metadata.drop_all(self.__engine)
+            Base.metadata.create_all(self.__engine)
 
     def all(self, cls=None):
         """Query objects from the database.
 
         Args:
-            cls (str or SQLAlchemy class, optional):class to query.
+            cls (str or SQLAlchemy class, optional): Class to query.
                 Defaults to None, which retrieves all objects from all classes.
 
         Returns:
-            dict: A dictionary of objects with format \
-                    '{ClassName}.{object_id}'.
+            dict: A dictionary of objects with format '{ClassName}.{object_id}'.
         """
         if cls is None:
-            objs = self.__session.query(State).all()
-            objs.extend(self.__session.query(City).all())
-            objs.extend(self.__session.query(User).all())
-            objs.extend(self.__session.query(Place).all())
-            objs.extend(self.__session.query(Review).all())
-            objs.extend(self.__session.query(Amenity).all())
+            objs = []
+            for model in [State, City, User, Place, Review, Amenity]:
+                objs.extend(self.__session.query(model).all())
+        elif isinstance(cls, str):
+            cls = eval(cls)
+            objs = self.__session.query(cls).all()
         else:
-            if isinstance(cls, str):
-                cls = eval(cls)
-            objs = self.__session.query(cls)
+            objs = self.__session.query(cls).all()
+
         return {"{}.{}".format(type(o).__name__, o.id): o for o in objs}
 
     def new(self, obj):
@@ -83,6 +83,18 @@ class DBStorage:
             obj (BaseModel): The object to add to the session.
         """
         self.__session.add(obj)
+
+    def get(self, cls, id):
+        """Retrieve an object based on class and ID"""
+        key = "{}.{}".format(cls.__name__, id)
+        return self.all(cls).get(key, None)
+
+    def count(self, cls=None):
+        """Count the number of objects in storage"""
+        if cls:
+            return self.__session.query(cls).count()
+        return sum(self.__session.query(model).count()
+                   for model in [State, City, User, Place, Review, Amenity])
 
     def save(self):
         """Commit changes to the current database session."""
@@ -100,11 +112,24 @@ class DBStorage:
     def reload(self):
         """Create all tables in the database and create a new session."""
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine,
-                                       expire_on_commit=False)
-        Session = scoped_session(session_factory)
-        self.__session = Session()
+        session_factory = sessionmaker(
+            bind=self.__engine, expire_on_commit=False)
+        self.__session = scoped_session(session_factory)
 
     def close(self):
         """Close the current session."""
-        self.__session.close()
+        self.__session.remove()
+
+    def _session(self):
+        """Get a new scoped session."""
+        if self.__session is None:
+            session_factory = sessionmaker(
+                bind=self.__engine, expire_on_commit=False)
+            self.__session = scoped_session(session_factory)
+        return self.__session()
+
+
+# Create an instance of DBStorage
+storage = DBStorage()
+# Load the data from the database
+storage.reload()
